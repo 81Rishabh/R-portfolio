f838d6ad0c559b2cdfc5cea487d37ed4817804df
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-10-20 19:42:20 +0530

    fixed some issue

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,22 +6,27 @@ function Navbar() {
   const [show, setshow] = useState(false);
   const [dark, setdark] = useState(false);
   
-  // set theme by initial loading
+  // set intial theme color
   useEffect(() => {
-
-  },[]);
-  
-  // 
-  function handleDarkMode() {
-    if(dark) {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
       document.documentElement.classList.add('dark');
-      localStorage.setItem('theme' , 'dark');
+      setdark(true);
+    } else {
+      document.documentElement.classList.remove('dark');
       setdark(false);
+    }
+  },[localStorage.theme]);
+
+  function handleDarkMode() {
+    if (!dark) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      setdark(true);
     }
     else {
       document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme' , 'light');
-      setdark(true);
+      localStorage.setItem('theme', 'light');
+      setdark(false);
     }
   }
 
@@ -66,22 +71,22 @@ function Navbar() {
             <span className="mx-2">Contact</span>
           </a>
           {
-            !dark ? (
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" dark:text-slate-300 w-6 h-6 cursor-pointer  rounded-full text-lg " onClick={handleDarkMode}>
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-            </svg>
-              ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-lg text-black cursor-pointer text-yellow-500" onClick={handleDarkMode}>
+            dark === false ? (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-lg text-black cursor-pointer text-yellow-500" onClick={handleDarkMode}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
               </svg>
-              )
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" dark:text-slate-300 w-6 h-6 cursor-pointer  rounded-full text-lg " onClick={handleDarkMode}>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+              </svg>
+            )
           }
         </div>
         <div className="hamburger cursor:pointer md:hidden relative">
           <svg onClick={() => setshow(!show)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-grey-500 hover:text-indigo-600 ">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
-          <Dropdown show={show} handleDarkMode={handleDarkMode}/>
+          <Dropdown show={show} handleDarkMode={handleDarkMode} />
         </div>
       </nav>
     </div>
